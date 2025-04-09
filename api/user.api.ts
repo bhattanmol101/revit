22a0fb0a8494c4257820b95a6bfbd93697aa738e
Anmol Bhat <bhattanmol101@gmail.com>
2025-04-09 15:05:32 +0530

    Fixing DB

--- api/user.api.ts
+++ api/user.api.ts
@@ -1,20 +1,19 @@
-import { UpdateUser, } from "@/types/user";
-
+import { UpdateUser } from "@/types/user";
 import { createClient } from "@/utils/supabase/server";
 
 export async function updateUser(userRequest: UpdateUser) {
-    try {
-      const supabase = await createClient();
+  try {
+    const supabase = await createClient();
 
-        const resp = await supabase.auth.updateUser({
-            data: userRequest
-        });
-  
-      return { success: true, error: "" };
-    } catch (e: any) {
-      return {
-        success: false,
-        error: e.message,
-      };
-    }
+    const resp = await supabase.auth.updateUser({
+      data: userRequest,
+    });
+
+    return { success: true, error: "" };
+  } catch (e: any) {
+    return {
+      success: false,
+      error: e.message,
+    };
   }
+}
